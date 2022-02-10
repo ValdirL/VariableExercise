adca668d6c539f1be199dd6c170c5711a8113ca5
Valdir Lima <valdirlima508@gmail.com>
2022-02-10 15:00:40 -0500

    Add project files.

---
+++ VariableExersize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariableExersize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
